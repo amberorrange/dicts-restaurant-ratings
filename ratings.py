afc95e4fc4fc260f89e60ee70f09e7db4a074356
Amber Orrange <orrangeamber@gmail.com>
2021-10-28 15:45:20 -0500

    finished part one

--- ratings.py
+++ ratings.py
@@ -1,4 +1,15 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+data = open("scores.txt")
+ratings_dict = {}
+
+for line in data:
+    name, rating = line.rstrip().split(":")
+
+    ratings_dict[name] = rating
+
+
+for dname in sorted(ratings_dict): 
+    print(f"{dname} is rated at {ratings_dict[dname]}.")
+    print()
