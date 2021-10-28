2b2249cb036d692887a70b46e249a9eaf2a17735
Amber Orrange <orrangeamber@gmail.com>
2021-10-28 15:53:33 -0500

    finished part 2

--- ratings.py
+++ ratings.py
@@ -6,10 +6,22 @@ ratings_dict = {}
 
 for line in data:
     name, rating = line.rstrip().split(":")
-
     ratings_dict[name] = rating
 
 
-for dname in sorted(ratings_dict): 
-    print(f"{dname} is rated at {ratings_dict[dname]}.")
-    print()
+def sort_restaurants(restaurants_dict):
+    for dname in sorted(restaurants_dict): 
+        print(f"{dname} is rated at {restaurants_dict[dname]}.")
+
+
+sort_restaurants(ratings_dict)
+
+
+user_restaurant = input("What restuarant do you want to score?  ")
+score = input( f"What score wiil you give to {user_restaurant}?  ")
+ratings_dict[user_restaurant] = score
+
+sort_restaurants(ratings_dict)
+
+
+
